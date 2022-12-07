281b1a07cf8a9dbad939bbc3a1b0095dd980d9d6
krishilsheth404 <97043368+krishilsheth404@users.noreply.github.com>
2022-12-07 08:39:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,208 +1,75 @@
-//dunzo parcel price getter automation
-const puppeteer = require('puppeteer')
 const express = require('express'); // Include ExpressJS
 const app = express(); // Create an ExpressJS app
 const bodyParser = require('body-parser'); // Middleware 
-const cheerio=require('cheerio');
-const fs = require('fs');
+const axios = require('axios')
+const cheerio = require('cheerio')
+// const puppeteer = require('puppeteer');
+// const request = require('request');
+// const fs = require('fs');
 const ejs = require("ejs");
-const { url } = require('inspector');
-const { fail } = require('assert');
+// const { AddressContext } = require('twilio/lib/rest/api/v2010/account/address');
+const { getElementsByTagType } = require('domutils');
 
 app.set('view engine', 'ejs');
-app.set('views', './');
+app.set('views',__dirname);
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }));  //*//
 app.use(bodyParser.json());
 // var newItem;
 // Route to Login Page
 app.get('/', (req, res) => {
+    // console.log(res)
     res.sendFile(__dirname + '/login.html');
 });
-app.post('/', (req, res) => {
-    res.sendFile(__dirname + '/login.html');
-});
-async function bigbasket(url) {
-    try {
-        // Fetching HTML
-        console.log(url);
-        const browser = await puppeteer.launch({
-            args : [ 
-               '--no-sandbox',
-               '--disable-setuid-sandbox',
-            ]
-        });;
-         const page = await browser.newPage();
-            await page.goto(url, { waitUntil: 'networkidle2' });
-            const data = await page.evaluate(() => document.querySelector('*').outerHTML);
-            console.log("got the data from bigbasket");
-            // await browser.close();
-            // console.log(data)
-            // await page.close();
+
+// app.post('/', (req, res) => {
+//     res.sendFile(__dirname + '/login.html');
+// });
+
+app.post('/result', async(req, res) => {
+
+    // app.get('/', (req, res) => {
+
+    const final = []
+
+    extractLinkFromyahoo = async(url) => {
+        try {
+            // Fetching HTML
+            const { data } = await axios.get(url)
+                // console.log(data)
+
             // Using cheerio to extract <a> tags
-            const $ = cheerio.load(data);
-        var n=$('.prod-name >a').first().text() +' '
-        n+= $('.qnty-selection div span').first().text();
-        // console.log($.html());
-        await browser.close();
-        return{
-            name:'BigBasket',
-            item:n,
-            price:$('.discnt-price').first().text(),
+            console.log(typeof(data));
+            const K = cheerio.load(data);  
+            console.log(typeof(K));
+            var t = 0;
+
+            K('.searchCenterMiddle li').find('li a').each(function(index, element) {
+                if (K(element).text() != 'Cached' && K(element).text() != '') {
+                    final.push({
+                        link: K(element).attr('href'),
+                        title: K(element).text(),
+                    });
+                }
+            });
+            console.log(final);
+
+        } catch (error) {
+            // console.log(error);
+            return 0;
         }
-        
-    } catch (error) {
-        // res.sendFile(__dirname + '/try.html');
-        console.log(error);
-    }
-}
-async function starquik(url){
-    try {
-        // Fetching HTML
-        console.log(url);
-        const browser = await puppeteer.launch({
-            args : [ 
-               '--no-sandbox',
-               '--disable-setuid-sandbox',
-            ]
-        });;
-         const page = await browser.newPage();
-            await page.goto(url, { waitUntil: 'networkidle2' });
-            const data = await page.evaluate(() => document.querySelector('*').outerHTML);
+    };
 
-            let name=await page.waitForSelector('.product-title');
-            name=await page.evaluate(el => el.textContent, name);
-        //   console.log(name);
-          
-          let price=await page.waitForSelector('.offer_price');
-            price=await page.evaluate(el => el.textContent, price);
+    const s = req.body.search + '\n';
+    console.log(s)
+    final.push(s);
 
-            //.product-img for image of product 
+    await extractLinkFromyahoo(`https://in.search.yahoo.com/search;_ylt=?p=${s}&ad=dirN&o=0`)
+    res.render(__dirname+'/final', { final: final })
 
-            // console.log("got the link for zomato");
-            // await browser.close();
-            // console.log(data)
-            // Using cheerio to extract <a> tags
-            // var n=$('.prod-name >a').first().text() +' '
-            // n+= $('.qnty-selection div span').first().text();
-            // console.log($.html());
-            await browser.close();
-        return{
-            name:'StarQuick',
-            item:name,
-            price:price,
-        }
-        
-    } catch (error) {
-        // res.sendFile(__dirname + '/try.html');
-        console.log(error);
-    }
-}
-async function jiomart(url){
-    try {
-        // Fetching HTML
-        console.log(url);
-        const browser = await puppeteer.launch({
-            args : [ 
-               '--no-sandbox',
-               '--disable-setuid-sandbox',
-            ]
-        });;
 
-         const page = await browser.newPage();
-            await page.goto(url, { waitUntil: 'networkidle2' });
-            const data = await page.evaluate(() => document.querySelector('*').outerHTML);
-
-            let name=await page.waitForSelector('.clsgetname');
-            name=await page.evaluate(el => el.textContent, name);
-        //   console.log(name);
-          
-          let price=await page.waitForSelector('#final_price');
-            price=await page.evaluate(el => el.textContent, price);
-
-          
-            await browser.close();
-        return{
-            name:'jioMart',
-            item:name,
-            price:price,
-        }
-        
-    } catch (error) {
-        // res.sendFile(__dirname + '/try.html');
-        console.log(error);
-    }
-};
-async function kimaye(url)
-{
-    try {
-        // Fetching HTML
-        console.log(url);
-        const browser = await puppeteer.launch({
-            args : [ 
-               '--no-sandbox',
-               '--disable-setuid-sandbox',
-            ]
-        });;
-        const page = await browser.newPage();
-        await page.goto(url, { waitUntil: 'networkidle2' });
-        const data = await page.evaluate(() => document.querySelector('*').outerHTML);
-        console.log("got the data from kimaye");
-        
-        // console.log(data)
-        // Using cheerio to extract <a> tags
-        const $ = cheerio.load(data);
-        // var n=$('.prod-name >a').first().text() +' '
-        // n+= $('.qnty-selection div span').first().text();
-        // console.log($.html());
-        // await broqwer.close();
-        await browser.close();
-        return{
-            name:'Kimaye',
-            item:$('.product-title').first().text(),
-            price:$('.price-Mumbai').first().text(),
-        }
-        
-    } catch (error) {
-        // res.sendFile(__dirname + '/try.html');
-        console.log(error);
-    }
-}
-
-app.post('/result', async (req, res) => {
-    // Insert Login Code Here
-    const nameOfFruit = req.body.fav + '\n';
-    const urlForBB = `https://www.bigbasket.com/ps/?q=${nameOfFruit}`
-    const urlForStar = `https://www.starquik.com/search/${nameOfFruit}`
-    const urlForJiomart=`https://www.jiomart.com/catalogsearch/result?q=${nameOfFruit}`;
-    const urlForKimaye=`https://kimaye.com/search?q=${nameOfFruit}&type=product`;
-
-    var final=[];
-   
-    const promise1=bigbasket(urlForBB);
-    const promise2=starquik(urlForStar);
-    const promise3=jiomart(urlForJiomart);
-    const promise4=kimaye(urlForKimaye);
-    await Promise.all([promise1, promise2, promise3,promise4]).then((values) => {
-        final.push(values[0])
-        final.push(values[1])
-        final.push(values[2])
-        final.push(values[3])
-      });
-    
-    // await Promise.all([final.push(await bigbasket(urlForBB)),final.push(await jiomart(urlForJiomart)),
-    //     final.push(await starquik(urlForStar)),final.push(await kimaye(urlForKimaye))])
-    
-    //     await Promise.all([final.push(await bigbasket(urlForBB)),final.push(await jiomart(urlForJiomart)),
-    //     final.push(await starquik(urlForStar)),final.push(await kimaye(urlForKimaye))])
-    
-    final.sort((a, b) => a.price - b.price); // b - a for reverse sort
-    console.log(final);
-    final.push(nameOfFruit)
-    res.render(__dirname+'/final', { final: final });
 });
-
-const port = process.env.PORT || 4000 // Port we will listen on
+const port = process.env.PORT || 1000 // Port we will listen on
 
 // Function to listen on the port
 app.listen(port, () => console.log(`This app is listening on port ${port}`));
